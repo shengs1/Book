ec6bf01c0ef1fa8363c643dcfe10d8975307fad6
shengs1 <thinhnguyen081805@gmail.com>
2023-10-13 16:48:52 +0700

    Update urls.py

--- booklist_project/urls.py
+++ booklist_project/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # URL quản trị Django
@@ -23,6 +24,7 @@ urlpatterns = [
 
 ]
 
+urlpatterns += staticfiles_urlpatterns()
 
 
 
